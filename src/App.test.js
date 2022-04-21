7f4156b58d66d1fe5689a33c65e5aa7ccf1e215a
SangMin <sangmin4208@gmail.com>
2022-04-21 18:17:33 +0900

    You're doing great! :+1:

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,9 @@ test('renders learn react link', () => {
   render(<App />) // App component를 virtual DOM에 생성
   // screen을 통해 Virtual Dom 탐색
   // const linkElement = screen.getByText(/learn react testing library/i) // regualr expression
-  const linkElement = screen.getByText('Learn React Testing Library')
+  const linkElement = screen.getByRole('link', {
+    name: /learn react testing library/i,
+  })
   // assertion - matcher
   expect(linkElement).toBeInTheDocument()
 })
