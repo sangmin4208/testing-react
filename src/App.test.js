a201bef1139b87198c4554ceddd4c7deb97d7104
SangMin <sangmin4208@gmail.com>
2022-04-21 17:27:08 +0900

    Update test from regular expression to literal string

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />) // App component를 virtual DOM에 생성
+  // screen을 통해 Virtual Dom 탐색
+  // const linkElement = screen.getByText(/learn react testing library/i) // regualr expression
+  const linkElement = screen.getByText('Learn React Testing Library')
+  // assertion - matcher
+  expect(linkElement).toBeInTheDocument()
+})
