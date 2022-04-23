879a138d3dbd3a489154acf63e2480251d0b9dc0
SangMin <sangmin4208@gmail.com>
2022-04-23 23:44:43 +0900

    You're doing great! ✍️

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,10 +4,8 @@ export const handlers = [
   rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
     return res(
       ctx.json([
-        ctx.json([
-          { name: 'Chocolate', imagePath: '/images/chocolate.png' },
-          { name: 'Vanilla', imagePath: '/images/vanilla.png' },
-        ]),
+        { name: 'Chocolate', imagePath: '/images/chocolate.png' },
+        { name: 'Vanilla', imagePath: '/images/vanilla.png' },
       ])
     )
   }),
